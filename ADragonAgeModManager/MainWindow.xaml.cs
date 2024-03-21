eea3d14f01702b1c511d17773682ed1670e17eca
Kenny Deschuyteneer <kenny.deschuyteneer@gmail.com>
2024-03-21 19:34:37 +0100

    big bang! 💥

---
+++ ADragonAgeModManager/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows;
+
+namespace ADragonAgeModManager;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+﻿# 🧩 A Dragon Age ModManager
+ 
+Source code for A Dragon Age ModManager. It's a working title.
+
+## License 
+ 
+[MIT](https://choosealicense.com/licenses/mit/) (for now?)
